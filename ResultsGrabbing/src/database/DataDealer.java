0b4dd7431ca8baab17876f50bb040a20d2449b62
Przemek <Przemek@KompStacjon>
2013-05-30 22:07:13 +0100

    DataDealer class added and/or modified. Error classes added. Needs testing.

--- ResultsGrabbing/src/database/DataDealer.java
+++ ResultsGrabbing/src/database/DataDealer.java
@@ -6,12 +6,15 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class DataWriter {
+import errors.DataDealerReadException;
+import errors.DataDealerWriteException;
+
+public class DataDealer {
 
 	Session session;
 	protected String configLocation = "E:\\Users\\Przemek\\Documents\\workspace-sts-3.2.0.RELEASE\\ResultsGrabbing\\hib\\hibernate.cfg.xml";
 	
-	public DataWriter(){
+	public DataDealer(){
 		SessionFactory factory = new Configuration().configure(new File(configLocation)).buildSessionFactory();
 		session = factory.openSession();
 
@@ -24,4 +27,19 @@ public class DataWriter {
 		return false;
 	}
 	
+	public int Write(Object obj) throws DataDealerWriteException{
+		if(session!=null && session.isOpen()){
+			session.saveOrUpdate(obj);
+			return 0;
+		}
+		throw new DataDealerWriteException();
+	}
+	
+	public Object readData(Class<?> cname, int id) throws DataDealerReadException{
+		if(session!=null && session.isOpen()){
+			return session.get(cname, new Integer(id));
+		}
+		throw new DataDealerReadException();
+		
+	}
 }
