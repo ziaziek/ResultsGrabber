66df9dac29f7aafc14d943734e547bb278fdae53
Przemo <Przemo@192.168.2.6>
2013-12-14 11:34:56 +0000

    Updater form checks for update requirements and informs if such is necesary.

--- ResultsGrabbing/src/gui/UpdateChecker.java
+++ ResultsGrabbing/src/gui/UpdateChecker.java
@@ -8,25 +8,45 @@ import data.Players;
 import database.DataDealer;
 import gubas.forms.Dialog;
 import gubas.forms.DialogForm;
+import gubas.javaapplication1.FormsCaller;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
 
 /**
  *
  * @author Przemo
  */
 public class UpdateChecker extends DialogForm {
+    boolean up = false;
+    final UpdateChecker me = this;
     
     public UpdateChecker(){
-        super( Dialog.OK, "Updates Checker");
-        try {
-            Thread.currentThread().wait(250);
-            if(checkUpdates("", (int) new DataDealer().rowsCount(Players.class)).equals("OK")){
-                this.dispose();
-            } //The class name parameter should be extended, so that every table should be checked
-        } catch (InterruptedException ex) {
-            Logger.getLogger(UpdateChecker.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        super( Dialog.OK, "Checking data status");
+        this.setPreferredSize(new Dimension(250,180));
+            new Thread(new Runnable(){
+
+                @Override
+                public void run() {
+                    up = checkUpdates("", (int) new DataDealer().rowsCount(Players.class)).equals("OK");//The class name parameter should be extended, so that every table should be checked
+                    me.updateCallback();
+                }
+        
+            }).start();
+
+    }
+    
+    void updateCallback(){
+        if(up){ 
+            this.addMessage("Status OK");
+        } else {
+            this.addMessage("Update required");
+        } 
+        me.repaint();
+        me.revalidate();
     }
     
     private static String checkUpdates(String lastDate, int noOfRecords) {
